2b155ace733a0a210e4ffcea7e69c233f40d61ba
Andre-Ovien <andreovien571@gmail.com>
2026-03-04 16:39:42 +0100

    project setup

---
+++ backend/Core/Core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/user/', include('Auth.urls')),
+    path("api/token/", TokenObtainPairView.as_view(), name="token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
